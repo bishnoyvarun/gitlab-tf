5da83ee821c2d667042bbccbe8f464115491cd48
Varun Kumar <varun31h@gmail.com>
2025-09-01 17:11:01 +0000

    Edit backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-#terraform {
-#  backend "s3" {
-#    bucket = "my-tf-test-bucket"
-#    key    = "backend.tfstate"
-#    region = "eu-north-1"
-#    dynamodb_table = "LockTable"
-#  }
-#}
+terraform {
+  backend "s3" {
+    bucket = "demosimplebucket-ed9f0d64527d15bd"
+    key    = "backend.tfstate"
+    region = "eu-north-1"
+    dynamodb_table = "LockTable"
+  }
+}
