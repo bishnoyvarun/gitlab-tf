cdedb6ba869edbf05a2ac50d9cdeb513c3639e15
Varun Kumar <varun31h@gmail.com>
2025-09-01 15:04:12 +0000

    Edit backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket = module.s3.mybucket
-    key    = module.s3.key
-    dynamodb_table       = module.db.dbtable
-    region = "eu-north-1"
-    encrypt = true
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = module.s3.mybucket
+#    key    = module.s3.key
+#    dynamodb_table       = module.db.dbtable
+#    region = "eu-north-1"
+#    encrypt = true
+#  }
+#}
