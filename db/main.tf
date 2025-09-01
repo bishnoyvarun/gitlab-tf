1d6f2279461425544336d30d9343c305798dc79a
Varun Kumar <varun31h@gmail.com>
2025-09-01 17:33:35 +0000

    Edit main.tf

--- db/main.tf
+++ db/main.tf
@@ -1,21 +1,21 @@
-# DynamoDB Table
-provider "aws" {
-  region  = "eu-north-1"
-  #profile = "vakky_devpos"
-}
+## DynamoDB Table
+#provider "aws" {
+#  region  = "eu-north-1"
+#  #profile = "vakky_devpos"
+#}
 
-resource "aws_dynamodb_table" "my_table" {
-  name           = "LockTable"
-  billing_mode   = "PAY_PER_REQUEST"  # On-demand pricing
-  hash_key       = "LockId"
+#resource "aws_dynamodb_table" "my_table" {
+#  name           = "LockTable"
+#  billing_mode   = "PAY_PER_REQUEST"  # On-demand pricing
+#  hash_key       = "LockId"
 
-  attribute {
-    name = "LockId"
-    type = "S"  # S = String, N = Number, B = Binary
-  }
+#  attribute {
+#    name = "LockId"
+#    type = "S"  # S = String, N = Number, B = Binary
+#  }
 
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
-}
+#  tags = {
+#    Environment = "dev"
+#    Terraform   = "true"
+#  }
+#}
