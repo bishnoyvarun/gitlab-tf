4569bedab25ed5d1b920c8a7689a742814bd1d54
Varun Kumar <varun31h@gmail.com>
2025-09-01 17:34:37 +0000

    Edit main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -1,17 +1,17 @@
-provider "aws" {
-  region  = "eu-north-1"
-  #profile = "vakky_devpos"
-}
+#provider "aws" {
+#  region  = "eu-north-1"
+#  #profile = "vakky_devpos"
+#}
 
-resource "aws_s3_bucket" "mysimplebucket" {
-  bucket = "my-tf-bucket0001" 
-   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
+#resource "aws_s3_bucket" "mysimplebucket" {
+#  bucket = "my-tf-bucket0001" 
+#   tags = {
+#    Name        = "My bucket"
+#    Environment = "Dev"
+#  }
+#}
 
-resource "aws_s3_object" "bucket-data" {
-  bucket = aws_s3_bucket.mysimplebucket.bucket
-  key = "backend.tfstate"
-}
+#resource "aws_s3_object" "bucket-data" {
+#  bucket = aws_s3_bucket.mysimplebucket.bucket
+#  key = "backend.tfstate"
+#}
