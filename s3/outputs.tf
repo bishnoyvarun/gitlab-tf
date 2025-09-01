3cc27d758db5c76827630eeff2d705e0c1633f2b
Varun Kumar <varun31h@gmail.com>
2025-09-01 17:35:05 +0000

    Edit outputs.tf

--- s3/outputs.tf
+++ s3/outputs.tf
@@ -1,9 +1,9 @@
-output "mybucket" {
-  value = aws_s3_bucket.mysimplebucket.bucket
-  
-}
+#output "mybucket" {
+#  value = aws_s3_bucket.mysimplebucket.bucket
+#  
+#}
 
-output "key" {
-  value = aws_s3_object.bucket-data.key
+#output "key" {
+#  value = aws_s3_object.bucket-data.key
   
-}
+#}
