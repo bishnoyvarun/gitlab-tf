94cd8395d064256e54ea067f1755b268bba689ea
Varun Kumar <varun31h@gmail.com>
2025-09-01 19:19:01 +0000

    Edit backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "demosimplebucket-e03613d8ddcc0f3f"
+    bucket = "demosimplebucket-5f70fcf255f8f675"
     key    = "backend.tfstate"
     dynamodb_table = "LockTable"
     region = "eu-north-1"
